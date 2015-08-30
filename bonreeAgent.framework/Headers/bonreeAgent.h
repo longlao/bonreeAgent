fc3b9296768477c19c3db445667857a6860a7581
jianghailong <395729207@qq.com>
2015-08-30 17:00:44 +0800

    bonreeAgent.framework

---
+++ bonreeAgent.framework/Headers/bonreeAgent.h
@@ -0,0 +1,19 @@
+//
+//  bonreeAgent.h
+//  bonreeAgent
+//
+//  Created by 蒋海龙 on 15/7/14.
+//  Copyright (c) 2015年 蒋海龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for bonreeAgent.
+FOUNDATION_EXPORT double bonreeAgentVersionNumber;
+
+//! Project version string for bonreeAgent.
+FOUNDATION_EXPORT const unsigned char bonreeAgentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <bonreeAgent/PublicHeader.h>
+
+#import "bonreeAgent/BRSAgent.h"
